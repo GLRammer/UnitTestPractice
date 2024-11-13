8997b0a49dd0cc5830078d83a3f1b81dea2eb216
Gary Layman <45434125+GLRammer@users.noreply.github.com>
2024-11-13 11:21:50 -0800

    test writing

--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -20,3 +20,45 @@ TEST(PasswordTest, single_letter_password)
 	int actual = my_password.count_leading_characters("Z");
 	ASSERT_EQ(1, actual);
 }
+
+TEST(PasswordTest, special_char_err){
+	Password myPassword;
+	bool actual = myPassword.has_mixed_case("A#a^ABoo");
+	ASSERT_EQ(true,actual);
+}
+
+TEST(PasswordTest, case_mixer){
+	Password myPassword;
+	bool actual = myPassword.has_mixed_case("AbadAsBoao");
+	ASSERT_EQ(true,actual);
+}
+
+TEST(PasswordTest, low_caser){
+	Password myPassword;
+	bool actual = myPassword.has_mixed_case("onwejfnkjnd");
+	ASSERT_EQ(false,actual);
+}
+
+TEST(PasswordTest, upper_caser){
+	Password myPassword;
+	bool actual = myPassword.has_mixed_case("JHBEFNDNM");
+	ASSERT_EQ(false,actual);
+}
+
+TEST(PasswordTest, mixed_case_leading){
+	Password myPassword;
+	int actual = myPassword.count_leading_characters("RrrRrRrR");
+	ASSERT_EQ(1,actual);
+}
+
+TEST(PasswordTest, all_five_leading){
+	Password myPassword;
+	int actual = myPassword.count_leading_characters("DDDDD");
+	ASSERT_EQ(5,actual);
+}
+
+TEST(PasswordTest, three_of_five_leading){
+	Password myPassword;
+	int actual = myPassword.count_leading_characters("lllfm");
+	ASSERT_EQ(3,actual);
+}
